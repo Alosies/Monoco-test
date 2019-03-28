8bc8b4126d6235a87c592ea843a7a85f1375f6b9
Alosies <alosies@gmail.com>
2019-03-28 19:18:31 +0530

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="wrapper">
+    <monaco-editor class="editor"
+                   v-model="code"
+                   language="yaml">
+    </monaco-editor>
+    <pre><code>{{ code }}</code></pre>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import MonacoEditor from "vue-monaco";
 
 export default {
-  name: "home",
   components: {
-    HelloWorld
+    MonacoEditor
+  },
+
+  data() {
+    return {
+      code:
+        'apiVersion: apps/v1\nkind: StatefulSet\nmetadata:\n name: web\nspec:\n serviceName: "nginx"\n replicas: 2\n selector:\n   matchLabels:\n     app: nginx\n template:\n   metadata:\n     labels:\n       app: nginx\n   spec:\n     containers:\n     - name: nginx\n       image: k8s.gcr.io/nginx-slim:0.8\n       ports:\n       - containerPort: 80\n         name: web'
+    };
   }
 };
 </script>
+
+<style>
+.editor {
+  width: 500px;
+  height: 400px;
+}
+</style>
+
+<style lang="stylus" scoped>
+.wrapper {
+  padding: 2rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+</style>
